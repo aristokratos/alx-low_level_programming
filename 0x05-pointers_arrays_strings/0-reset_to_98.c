c2289cd62e288f2c798609ec8e7049d6483bb24a
aristokratos <stefaneey@hmail.com>
2022-03-21 06:27:41 -0700

    question zero

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,27 +1,13 @@
 #include "main.h"
 
-
 /**
- * -reset_to_98  takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
  *
- * @c: character to be checked
- *
- * Return: 1 if c is capital, otherwise 0
- **/
+ * Return: nothing
+ */
 
-int main () {
-
-   int  var = 98;
-   int  *ip;
-
-   ip = &var;
-
-int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-
-   return 0;
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
