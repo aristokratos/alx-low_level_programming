8fb62435f2704edc5898d23d79dd0fc5033ca195
aristokratos <stefaneey@hmail.com>
2022-04-08 02:33:54 -0700

    More Malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
  * string_nconcat - concatenates two stringa.
  * @s1: string to be concatenated.
@@ -8,29 +8,41 @@
  * @n: first bytes of s2.
  * Return: String concatenated.
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *array = NULL;
-unsigned int i = 0, j = 0, k;
+	char *str;
+	char *nul = "";
+	unsigned int x, y;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i] != '\0')
-i++;
-while (s2[j] != '\0')
-j++;
-if (n < j)
-j = n;
-array = malloc(((i + j + n) * sizeof(char) + 1));
-if (array == NULL)
-return (NULL);
-for (k = 0; k < i; k++)
-array[k] = s1[k];
-i = k;
-for (k = 0; k < j; k++, i++)
-array[i] = s2[k];
-array[i] = '\0';
-return (array);
+	x = y = 0;
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
+	while (s1[x] != '\0')
+		x += 1;
+	while (s2[y] != '\0')
+		y += 1;
+	y++;
+	if (n >= y)
+		n = y;
+	str = malloc(sizeof(*str) * n + (x + 1));
+	if (str == NULL)
+		return (NULL);
+	x = 0;
+	while (s1[x] != '\0')
+	{
+		str[x] = s1[x];
+		x += 1;
+	}
+	y = 0;
+	while (s2[y] != '\0' && y < n)
+	{
+		str[x] = s2[y];
+		x++;
+		y++;
+	}
+	str[x] = '\0';
+	return (str);
 }
